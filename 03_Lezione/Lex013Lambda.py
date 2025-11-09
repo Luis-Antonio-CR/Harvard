fc14db336ca31b700158dba99e13c30be9f8ee79
Luis-Antonio-CR <luisantonio9893@gmail.com>
2025-11-09 11:03:34 +0100

    Create Lambra.py. Error in sort a list of dict.

---
+++ 03_Lezione/Lex013Lambda.py
@@ -0,0 +1,10 @@
+people = [
+    {"name": "Asta", "squad": "Black Bulls"},
+    {"name": "Yuno", "squad": "Golden Dawn"},
+    {"name": "Noelle", "squad": "Black Bulls"},
+    {"name": "Mimosa", "squad": "Golden Dawn"}
+]
+
+people.sort()
+
+print(people)
