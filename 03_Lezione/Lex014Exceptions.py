d3a1b67a94de51a086c09e65175a1ec1ae222a32
Luis-Antonio-CR <luisantonio9893@gmail.com>
2025-11-09 11:16:52 +0100

    Using the try - except utility in py with an exception of ZeroDivisionError.

--- 03_Lezione/Lex014Exceptions.py
+++ 03_Lezione/Lex014Exceptions.py
@@ -1,6 +1,13 @@
+import sys
+
 x = int(input("x: "))
 y = int(input("y: "))
 
-result = x / y
+try:
+    result = x / y
+except ZeroDivisionError:
+    print("Error: Cannot divide by zero.")
+    sys.exit(1)
+
 
 print(f"{x} / {y} result in {result}.")
