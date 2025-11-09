2adf48fe97780ca37e98a4cd52e3dec7b8655a69
Luis-Antonio-CR <luisantonio9893@gmail.com>
2025-11-09 10:58:25 +0100

    Create decorations.py. Try create a decoration function for another function.

---
+++ 03_Lezione/Lex012Decorators.py
@@ -0,0 +1,14 @@
+#Function are values like a str
+
+def announce(f):
+    def wrapper():
+        print("About to run the function...")
+        f()
+        print("Done with the function.")
+    return wrapper
+
+@announce
+def hello():
+    print("Hello, world!")
+
+hello()
