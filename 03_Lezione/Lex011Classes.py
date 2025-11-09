4d15aac8f6329e40cac6d326e5a729b3c7f8f97d
Luis-Antonio-CR <luisantonio9893@gmail.com>
2025-11-09 10:50:33 +0100

    Create Flight class with three functions init, open_seats, add_passengers.

--- 03_Lezione/Lex011Classes.py
+++ 03_Lezione/Lex011Classes.py
@@ -7,3 +7,27 @@ p = Point(2, 8)
 
 print(p.x)
 print(p.y)
+
+class Flight():
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.passengers = []
+
+    def open_seats(self):
+        return self.capacity - len(self.passengers)
+
+    def add_passenger(self, name):
+        if not self.open_seats():
+            return False    
+        self.passengers.append(name)
+        return True
+
+flight = Flight(3)
+peoples = ["Asta", "Yuno", "Noelle", "Mimosa"]
+
+for person in peoples:
+    success = flight.add_passenger(person)
+    if success:
+        print(f"Added {person} to flight successfully")
+    else:
+        print(f"No available seats for {person}")
